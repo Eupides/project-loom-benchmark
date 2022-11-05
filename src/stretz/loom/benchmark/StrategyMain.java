4bdaeb301dbf8d747902207cad576ec3d4ff8a4d
Alexander Stretz <stretz.alexander@gmail.com>
2022-11-05 20:09:01 +0100

    added cputask to main program

--- src/stretz/loom/benchmark/StrategyMain.java
+++ src/stretz/loom/benchmark/StrategyMain.java
@@ -1,11 +1,17 @@
 package stretz.loom.benchmark;
 
+import stretz.loom.benchmark.tasks.CPUTask;
 import stretz.loom.benchmark.tasks.IoTask;
+import stretz.loom.benchmark.tasks.cpu.CalculationStrategy;
+import stretz.loom.benchmark.tasks.cpu.SimpleCalculation;
 
 public class StrategyMain {
     public static void main (String[] args) {
         IoTask taskOne = new IoTask(100, 1);
-
         taskOne.execute();
+
+        CalculationStrategy strat = new SimpleCalculation(1000, Integer.MAX_VALUE, 10);
+        CPUTask taskTwo = new CPUTask(strat, 10000000);
+        taskTwo.execute();
     }
 }
